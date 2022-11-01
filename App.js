4afab7364146fb3af565ed27f1641947c32b4d3e
kanika0104 <116813643+kanika0104@users.noreply.github.com>
2022-11-01 13:21:10 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import { Fragment } from 'react';
+import Home from './Components/Home';
+import About from './Components/About';
+import Blog from './Components/Blog';
+import Contact from './Components/Contact';
+function App() {
+  return (
+   <Fragment>
+     <Router>
+     <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/blog" element={<Blog/>}/>
+     <Route path="/contact" element={<Contact/>}/>
+     </Routes>
+     </Router>
+   </Fragment>
+  );
+}
+
+export default App;
