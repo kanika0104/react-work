a9696c34a06915f931039cea2f67a91ccf307ded
kanika0104 <116813643+kanika0104@users.noreply.github.com>
2022-11-01 17:43:28 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import { Fragment } from 'react';
 import Home from './Components/Home';
 import About from './Components/About';
 import Blog from './Components/Blog';
 import Contact from './Components/Contact';
+
 function App() {
   return (
    <Fragment>
